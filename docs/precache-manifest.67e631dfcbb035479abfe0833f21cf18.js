3974fe8ed81535f7202505c16a5b449a44dc3407
Rainbow Asteroids <rainbowasteroids@protonmail.com>
2019-12-21 20:52:42 -0500

    Add static site for Github Pages

---
+++ docs/precache-manifest.67e631dfcbb035479abfe0833f21cf18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7efd848e106d8804de75e289717221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22f448dc2145af173c39",
+    "url": "/static/css/main.743129a4.chunk.css"
+  },
+  {
+    "revision": "abcd235990cf31208491",
+    "url": "/static/js/2.115af6a9.chunk.js"
+  },
+  {
+    "revision": "80f887ecd8e7f2b705b21a0a76fe0e53",
+    "url": "/static/js/2.115af6a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22f448dc2145af173c39",
+    "url": "/static/js/main.d75376ef.chunk.js"
+  },
+  {
+    "revision": "ed9fdc3cfc4c4ad8be3c",
+    "url": "/static/js/runtime-main.9cbcc9d2.js"
+  }
+]);
