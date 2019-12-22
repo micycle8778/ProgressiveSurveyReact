d0014fb6dd00dcb8cce1d4fe8547f5bb2325f70d
Rainbow Asteroids <rainbowasteroids@protonmail.com>
2019-12-22 00:27:56 -0500

    Fixed BrowserRouter for deployment

---
+++ docs/precache-manifest.bf7863ac3b8e62b4ec5ecda99b016f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3745f041978fb34572b33ce6cce8c962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fd026aa367a051faae7",
+    "url": "/static/css/main.743129a4.chunk.css"
+  },
+  {
+    "revision": "abcd235990cf31208491",
+    "url": "/static/js/2.115af6a9.chunk.js"
+  },
+  {
+    "revision": "80f887ecd8e7f2b705b21a0a76fe0e53",
+    "url": "/static/js/2.115af6a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5fd026aa367a051faae7",
+    "url": "/static/js/main.b4972d35.chunk.js"
+  },
+  {
+    "revision": "ed9fdc3cfc4c4ad8be3c",
+    "url": "/static/js/runtime-main.9cbcc9d2.js"
+  }
+]);
